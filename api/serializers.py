3b61e2950621d98799319c4ed4ffbbe7f3b1a77b
Hossein <hossein.gasemzadeh03@gmail.com>
2024-11-01 20:33:47 +0330

    feat(serializers): add author, genre, and book serializers
    - Introduced AuthorSerializer for creating and validating authors.
    - Added GenreSerializer for managing genre data.
    - Implemented BookSerializer that supports nested author and genre.
    - Enhanced create and update methods in BookSerializer for better data handling.

--- api/serializers.py
+++ api/serializers.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-
+from .models import *
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
@@ -48,4 +48,68 @@ class UserLoginSerializer(serializers.Serializer):
             'user': user,
             'message': 'Login successful!',
         }
+
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['id', 'first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    
+    def validate_first_name(self, value):
+        if not value:
+            raise ValidationError("First name cannot be empty.")
+        return value
+
+    def validate_last_name(self, value):
+        if not value:
+            raise ValidationError("Last name cannot be empty.")
+        return value
+
+
+    def create(self, validated_data):
+        author, _ = Author.objects.get_or_create(**validated_data)
+        return author
+
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ['name']
+
+
+class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(many=True)
+    genre = GenreSerializer(many=True)
+    
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'summary', 'genre', 'published_date', 'page_count', 'cover_image']
+    
+    
+    def create(self, validated_data):
+        author_data = validated_data.pop('author')
+        genre_data = validated_data.pop('genre')
+        
+        author, _ = Author.objects.get_or_create(**author_data)
+        genre, _ = Genre.objects.get_or_create(**genre_data)
+    
+        book = Book(**validated_data)
+        book.author = author
+        book.genre = genre
+        book.save()
+    
+        return book
+    
+ 
+    def update(self, instance, validated_data):
+
+        instance.title = validated_data.get('title', instance.title)
+        instance.summary = validated_data.get('summary', instance.summary)
+        instance.published_date = validated_data.get('published_date', instance.published_date)
+        instance.page_count = validated_data.get('page_count', instance.page_count)
+        instance.cover_image = validated_data.get('cover_image', instance.cover_image)
+ 
+        return instance
 
